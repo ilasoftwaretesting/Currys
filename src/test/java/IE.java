b837552fffaa61d98bbcf26feb00fb04df344fe5
ilasoftwaretesting <ilasoftwaretesting@gmail.com>
2020-06-24 17:53:01 +0100

    Homework CurrysProject

---
+++ src/test/java/IE.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class IE {
+
+    static WebDriver driver;
+
+    public static void main (String [] args){
+
+        System.setProperty("webdriver.ie.driver","C:\\Soft\\IEDriverServer.exe");
+        driver = new InternetExplorerDriver();
+        driver.get("https://www.currys.com/");
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.close();
+
+    }
+
+}
+
+
+
